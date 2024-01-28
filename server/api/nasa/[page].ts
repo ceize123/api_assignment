b4fcfc0f762cd9999b5819fb737fe90eb1c96b79
Enzo <yitsokuo19@gmail.com>
2024-01-28 12:23:52 -0500

    url bug fix

--- server/api/nasa/[page].ts
+++ server/api/nasa/[page].ts
@@ -6,5 +6,14 @@ export default defineEventHandler(async (event) => {
     'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000';
   const url = `${baseUrl}&page=${page}&api_key=${apiKey}`;
   const data = (await $fetch(url)) as any;
+
+  // Check if the response contains photos with URLs
+  if (data.photos) {
+    // Update photo URLs to use HTTPS
+    data.photos = data.photos.map((photo: any) => {
+      photo.img_src = photo.img_src.replace('http://', 'https://');
+      return photo;
+    });
+  }
   return data.photos;
 });
